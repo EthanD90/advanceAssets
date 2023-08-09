0355c33edd166e9438efa364e697bb7220f49d3c
EthanD90 <62568992+EthanD90@users.noreply.github.com>
2023-08-09 09:07:26 +0100

    update serial no regex in data_cleaner.py

--- data_cleaner.py
+++ data_cleaner.py
@@ -6,7 +6,7 @@ def clean_advance_data(dataframe):
     df['Manufacturer'] = df['Equipment'].str.extract(r'^(.*?) -')
     df['Product'] = df['Equipment'].str.extract(r'-(.*?)(?= Asset No)')
     df['Asset No'] = df['Equipment'].str.extract(r'Asset No: (\d+)')
-    df['Serial No'] = df['Equipment'].str.extract(r'Serial No: ([\w\d]+)')
+    df['Serial No'] = df['Equipment'].str.extract(r'Serial No: ([\w\d\s\-_!@#$%^&*()+=?<>{}|~.,]+)')
     df['Tags'] = df['Unnamed: 1']
     df['Age (Years)'] = pd.to_numeric(df['Age (Years)'])
     df['All Calls'] = pd.to_numeric(df['All Calls'])
